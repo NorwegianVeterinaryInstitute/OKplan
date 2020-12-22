1d7cc63bed120cd697dc37e55cf7422c224464c8
Petter Hopp <Petter.Hopp@vetinst.no>
2020-12-22 07:51:37 +0100

    doc: templates for vignette code

---
+++ notes/vignette_OK_template_save_as Excel_using_functions.R
@@ -0,0 +1,56 @@
+library(NVIdb)
+library(NVIpretty)
+library(openxlsx)
+
+
+plan_aar <- 2021
+
+colclasses <- standardize_columns(csvfile = paste0(set_dir_NVI("OKprogrammer"), "Rutine", plan_aar, "/planlegging/resultater/utvalgslister/OKWEB_alle_gris.csv"),
+                                  dbsource = "okplan",
+                                  property = "colclasses")
+okplan <- read.csv2(file = paste0(set_dir_NVI("OKprogrammer"), "Rutine", plan_aar, "/planlegging/resultater/utvalgslister/OKWEB_alle_gris.csv"),
+                    colClasses = colclasses,
+                    fileEncoding = "UTF-8")
+# data <- gris_virus_slaktegris_utvalg
+# dbsource <- "gris_virus_slaktegris_utvalg"
+
+# Slaktegrislister
+gris_virus_slaktegris_utvalg <- okplan %>%
+  dplyr::filter(ok_hensiktkode == "0200102" & statuskode == 1 & ok_driftsformkode == "10202") %>%
+  dplyr::mutate(rapport = "Spesifikke virusinfeksjoner hos slaktegrisbesetninger, utvalgsliste") %>%
+  dplyr::arrange(mt_regionnr, mt_avdelingnr, eier_lokalitetnr)
+
+# Standardiserer kolonnenavn
+gris_virus_slaktegris_utvalg <- standardize_columns(gris_virus_slaktegris_utvalg, property = "colnames")
+
+# Plasserer kolonnene i fastsatt rekkefølge og fjerner overflødige kolonner
+  gris_virus_slaktegris_utvalg  <- standardize_columns(gris_virus_slaktegris_utvalg, property = "colorder", exclude = TRUE)
+
+
+  # ENDRE TIL FUNKSJON
+  # Inkludere en tom rad og en rad som angir når datauttrekket ble gjort
+  gris_virus_slaktegris_utvalg <- as.data.frame(gris_virus_slaktegris_utvalg)
+  gris_virus_slaktegris_utvalg[(dim(gris_virus_slaktegris_utvalg)[1] + 1):(dim(gris_virus_slaktegris_utvalg)[1] + 2), ] <- NA
+  gris_virus_slaktegris_utvalg[dim(gris_virus_slaktegris_utvalg)[1], 1] <- paste("Datauttrekket er gjort ", format(Sys.Date(),"%d/%m/%Y"))
+
+
+# Generere Excel-ark
+OK_wb <- createWorkbook()
+# colnames(antprover_slakteri) <- standardize_columns(data = antprover_slakteri, property = "collabels")
+
+
+add_formatted_worksheet(gris_virus_slaktegris_utvalg,
+                        OK_wb,
+                        sheet = paste0("Slaktegris_utvalgt_", plan_aar),
+                        wrapHeadlineText = TRUE,
+                        collabels = TRUE,
+                        colwidths = TRUE)
+
+
+
+
+saveWorkbook(wb = OK_wb,
+             file = paste0(set_dir_NVI("OKprogrammer"), "Rutine", plan_aar, "/planlegging/Utvalgslister/Test ", plan_aar, " utvalgte besetninger.xlsx"),
+             overwrite = TRUE)
+
+
