39eb251c291f5f02db9e9de66689d7ba60a0396b
Petter Hopp <Petter.Hopp@vetinst.no>
2021-11-29 14:10:10 +0100

    refactor: used dplyr instead of poorman in generate_OK_column_standards

--- data-raw/generate_OK_column_standards.R
+++ data-raw/generate_OK_column_standards.R
@@ -20,29 +20,29 @@ library(usethis)
 # READS AND TRANSFORMS EXCEL SHEET WITH COLUMN STANDARD INFORMATION ----
 OK_column_standards <- read.xlsx(xlsxFile = paste0(set_dir_NVI("ProgrammeringR"), "standardization/colnames/colnames_translation_table.xlsx")) %>%
   # Selects only information used in OK-planning
-  poorman::filter(db == "OK_planlegging") %>%
+  dplyr::filter(db == "OK_planlegging") %>%
   # Generates column labels based on label and spec for no and en
   # Use exactly same transformations as for column_standards
-  poorman::rename(label_no = collabel_no, label_en = collabel_en) %>%
-  poorman::mutate(label_1_no = poorman::case_when(is.na(spec_no) ~ label_no,
+  dplyr::rename(label_no = collabel_no, label_en = collabel_en) %>%
+  dplyr::mutate(label_1_no = dplyr::case_when(is.na(spec_no) ~ label_no,
                                               spec_no %in% c("dato", "geometrisk middel 3") ~ paste(label_no, spec_no),
                                               spec_no %in% c("kg", "kjennelse", "tid") ~ label_no,
                                               spec_no %in% c("antall undersøkt") ~ paste(spec_no, label_no),
                                               TRUE ~ spec_no)) %>%
-  poorman::mutate(label_1_en = poorman::case_when(is.na(spec_en) ~ label_en,
+  dplyr::mutate(label_1_en = dplyr::case_when(is.na(spec_en) ~ label_en,
                                               spec_en %in% c("date") ~ paste(label_en, spec_en),
                                               spec_en %in% c("kg", "time", "determination") ~ label_en,
                                               spec_en %in% c("No. tested") ~ paste(spec_en, label_en),
                                               TRUE ~ spec_en)) %>%
   # selects, renames and orders columns
-  poorman::select(db, table_db, colname_db, colname, label_1_no, label_no, spec_no, label_1_en, label_en, spec_en,
+  dplyr::select(db, table_db, colname_db, colname, label_1_no, label_no, spec_no, label_1_en, label_en, spec_en,
                 colwidth_Excel = colwidth_excel, colwidth_DT = colwidth_dt_tables, colclasses, colorder) %>%
-  poorman::distinct()
+  dplyr::distinct()
 
 
 # Generate data frame with all table names
 db_tables <- as.data.frame(unique(OK_column_standards$table_db)) %>%
-  poorman::rename(tables = 1) %>%
+  dplyr::rename(tables = 1) %>%
   dplyr::mutate(table = strsplit(tables, split = ",")) %>%
   dplyr::mutate_if(is.list, purrr::simplify_all) %>%    # flatten each list element internally
   tidyr::unnest(cols = "table")# expand
@@ -59,9 +59,9 @@ db_tables <- as.data.frame(unique(OK_column_standards$table_db)) %>%
 
 # Generate table with each table name on one line
 OK_column_standards <- OK_column_standards %>%
-  poorman::left_join(db_tables, by = c("table_db" = "tables")) %>%
-  poorman::mutate(table_db = trimws(table)) %>%
-  poorman::select(!table)
+  dplyr::left_join(db_tables, by = c("table_db" = "tables")) %>%
+  dplyr::mutate(table_db = trimws(table)) %>%
+  dplyr::select(!table)
 
 # unique_colnames <- unique(column_standards[, c("colname_db", "colname")]) %>%
 #   poorman::add_count(colname_db, name = "unique_colnames") %>%
