7f85fd2cd3c311cf467f4d8f7f05bb47b4c11f8b
Petter Hopp <Petter.Hopp@vetinst.no>
2021-12-09 15:38:39 +0100

    style: test_append_date_generated styled with spaces

--- tests/testthat/test_append_date_generated_line.R
+++ tests/testthat/test_append_date_generated_line.R
@@ -13,15 +13,14 @@ x <- as.data.frame(cbind("År" = 2021, "Rapport" = "Brucellose hos geit, utvalgs
 y <- append_date_generated_line(x)
 
 expect_equal(dim(y)[1], dim(x)[1] + 2)
-expect_identical(y[dim(y)[1], 1], paste("Datauttrekket er gjort", format(Sys.Date(),"%d/%m/%Y")))
+expect_identical(y[dim(y)[1], 1], paste("Datauttrekket er gjort", format(Sys.Date(), "%d/%m/%Y")))
 
 
 # Include row with generated date
-today <- format(Sys.Date(),"%d/%m/%Y")
+today <- format(Sys.Date(), "%d/%m/%Y")
 y <- append_date_generated_line(x, pretext = "Data was generated", date = today)
 
 expect_equal(dim(y)[1], dim(x)[1] + 2)
-expect_identical(y[dim(y)[1], 1], paste("Data was generated", format(Sys.Date(),"%d/%m/%Y")))
+expect_identical(y[dim(y)[1], 1], paste("Data was generated", format(Sys.Date(), "%d/%m/%Y")))
 
 })
-
