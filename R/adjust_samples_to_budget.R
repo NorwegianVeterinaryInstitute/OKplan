248f50f2d471acf0fa6877d847d639b7941db98a
Petter Hopp <petter.hopp@vetinst.no>
2022-11-17 15:42:08 +0100

    feat: developing adjust_samples_to_budget to include argument group

--- R/adjust_samples_to_budget.R
+++ R/adjust_samples_to_budget.R
@@ -6,7 +6,8 @@
 #' @details The sample size should first be estimated by percentage or similar.
 #'
 #' @param data Data frame
-#' @param budget The total budgeted sample number.
+#' @param group \[character\]. Vector with group variables.
+#' @param budget The total budgeted sample number or a column in data with the number of samples.
 #' @param sample_to_adjust The name of the column with the sample number per unit that should be adjusted.
 #' @param adjusted_sample The name of the column with the adjusted sample number per unit.
 #' @param adjust_by The maximum number of samples that one should adjust by.
@@ -16,6 +17,7 @@
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #' @export
 #' @examples
+#' library(OKplan)
 #' # Add data frame with sample number to adjust
 #' x <- as.data.frame(cbind(c(1:10),
 #'                          c(24, 30, 36, 12, 6, 18, 6, 0, 0, 0)))
@@ -27,14 +29,17 @@
 #'                               sample_to_adjust = "sample",
 #'                               adjusted_sample = "new_sample",
 #'                               adjust_by = 4)
-adjust_samples_to_budget <- function(data, budget, sample_to_adjust, adjusted_sample = "justert_ant_prover", adjust_by) {
+adjust_samples_to_budget <- function(data, group = NULL, budget, sample_to_adjust, adjusted_sample = "justert_ant_prover", adjust_by) {
 
   # ARGUMENT CHECKING ----
   # Object to store check-results
   checks <- checkmate::makeAssertCollection()
   # Perform assertions
   checkmate::assert_data_frame(x = data, all.missing = FALSE, add = checks)
-  checkmate::assert_integerish(budget, lower = 1, len = 1, any.missing = FALSE, add = checks)
+  checkmate::assert_subset(group, choices = colnames(data), add = checks)
+  checkmate::assert(checkmate::check_integerish(budget, lower = 1, len = 1, any.missing = FALSE),
+                    checkmate::check_choice(budget, choices = colnames(data)),
+                    add = checks)
   checkmate::assert_choice(sample_to_adjust, choices = colnames(data), add = checks)
   checkmate::assert_character(adjusted_sample, len = 1, min.chars = 1, any.missing = FALSE, add = checks)
   checkmate::assert_integerish(adjust_by, lower = 1, len = 1, any.missing = FALSE, add = checks)
@@ -42,21 +47,33 @@ adjust_samples_to_budget <- function(data, budget, sample_to_adjust, adjusted_sa
   checkmate::reportAssertions(checks)
 
   # INITILIZE VARIABLES ----
+  ERROR
   total_estimated <- sum(data[, sample_to_adjust], na.rm = TRUE)
   n_units <- length(which(data[, sample_to_adjust] > 0))
-  difference <- c(as.numeric(total_estimated - budget), rep(NA, dim(data)[1] - 1))
+  if (budget %in% colnames(data)) {
+    budget <- data[, budget]
+  } else {
+    budget <- rep(budget, dim(data)[1])
+  }
+  #  difference <- c(as.numeric(total_estimated - budget), rep(NA, dim(data)[1] - 1))
+  difference <- rep(as.numeric(total_estimated), dim(data)[1]) - budget
 
   # ADJUST SAMPLE NUMBER ----
   # Order data with largest sample size first
-  data <- data[order(data[, sample_to_adjust], decreasing = TRUE), ]
+                           # data <- data[order(data[, sample_to_adjust], decreasing = TRUE), ]
+  if (!is.null(group)) {
+    data <- sort(data, c(group, sample_to_adjust), decreasing = TRUE)
+  } else {data <- sort(data, sample_to_adjust, decreasing = TRUE)}
 
   # If total_estimated = budget, make new column adjusted_sample based on sample_to_adjust
-  if (total_estimated == budget) {
+  # if (total_estimated == budget) {
+  if (all(difference == 0)) {
     data[, adjusted_sample] <- data[, sample_to_adjust]
   }
 
-    # Only justify sample number when there is disagreement between budget and calculated number of samples
-  if (total_estimated != budget) {
+  # Only justify sample number when there is disagreement between budget and calculated number of samples
+  #  if (total_estimated != budget) {
+  if (any(difference != 0)) {
     # Adjust for each sampled unit with the unit having the largest sample size first
     for (i in c(1:dim(data)[1])) {
 
@@ -76,7 +93,9 @@ adjust_samples_to_budget <- function(data, budget, sample_to_adjust, adjusted_sa
       # Make new column with adjusted number
       data[i, adjusted_sample] <- data[i, sample_to_adjust] + justify
       if (i < dim(data)[1]) {
-        difference[i + 1] <- difference[i] + justify
+        if (is.null(group) || identical(data[i, group], data[i + 1, group])) {
+          difference[i + 1] <- difference[i] + justify
+        }
       }
 
     }
