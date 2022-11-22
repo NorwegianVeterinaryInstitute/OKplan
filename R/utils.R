3237c2cee8f996613d42ab38c1ee9e9a9f06cdb4
Petter Hopp <Petter.Hopp@vetinst.no>
2022-11-22 08:03:05 +0100

    refactor: sort.data.frame removed to aviod problems with licences.

--- R/utils.R
+++
@@ -1,8 +0,0 @@
-# Sort data frames
-# benefits from sort being S3-function
-
-sort.data.frame <- function(x, decreasing = FALSE, by = 1, ...) {
-  f <- function(...) order(..., decreasing = decreasing)
-  i <- do.call(f, x[by])
-  x[i, , drop = FALSE]
-}
