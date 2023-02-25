59916af19b88a470d10f431cbab6ecf7e29d02a8
Petter Hopp <petter.hopp@vetinst.no>
2023-02-25 16:22:49 +0100

    style: utils styled

--- R/utils.R
+++ R/utils.R
@@ -5,16 +5,15 @@ add_per_group <- function(data, values, group, new_column, FUN, keep_order = FAL
 
   colnames(df_agg)[c((length(group) + 1):(length(group) + length(values)))] <- new_column
 
-  if(keep_order) {data$agg_original_sort_order <- seq_len(nrow(data))}
+  if (keep_order) {data$agg_original_sort_order <- seq_len(nrow(data))}
 
   data <- merge(data, df_agg, by = group, all.x = TRUE)
 
   # Sorts data in original order and removes sort key
-  if(keep_order) {
+  if (keep_order) {
     data <- data[order(data$agg_original_sort_order), ]
     data$agg_original_sort_order <- NULL
   }
 
   return(data)
 }
-
