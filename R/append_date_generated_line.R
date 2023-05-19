c06f7f17c5e3495717e5bda272d1358bff91903a
Petter Hopp <petter.hopp@vetinst.no>
2023-05-19 22:33:42 +0200

    doc: improved help for append_date_generated_line

--- R/append_date_generated_line.R
+++ R/append_date_generated_line.R
@@ -1,13 +1,19 @@
-#' @title Add new last row with generated date
-#' @description Function to add new row with generated date. Before the date the pretext is "Datauttrekket ble gjort"
+#' @title Add new last row with the generated date
+#' @description Function to add new row with the generated date. Before the date
+#'     a pretext can be set.
 #'
-#' @details Two rows are dded to the data frame, the first is empty, the second has the generated date in the first column. The
+#' @details Two rows are added to the data frame, the first is empty, the second
+#'     has the generated date in the first column.
 #'
-#' @param data Data frame with data that should get new row with generated date
-#' @param pretext The explaining text before the date value.
-#' @param date Date for generating the data
+#' @param data [\code{data.frame}]\cr
+#'     The data to which an additional row with the generate date should be added.
+#' @param pretext [\code{character}]\cr
+#'     The explaining text before the date value. Defaults to "Datauttrekket ble gjort".
+#' @param date [\code{character}]\cr
+#'     Date for generating the data. Defaults to
+#'     \ifelse{html}{\code{\link[base]{Sys.Date}}}{\code{base::Sys.Date}}.
 #'
-#' @return A data frame with two more rows, one empty and one with generated data in the first column.
+#' @return \code{data.frame} with two additional rows, one empty and one with the generated date in the first column.
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #' @export
@@ -17,16 +23,23 @@
 #' gris_virus_slaktegris_utvalg <- append_date_generated_line(gris_virus_slaktegris_utvalg)
 #' }
 #'
-append_date_generated_line <- function(data, pretext = "Datauttrekket er gjort", date = format(Sys.Date(), "%d/%m/%Y")) {
+append_date_generated_line <- function(data,
+                                       pretext = "Datauttrekket er gjort",
+                                       date = format(Sys.Date(), "%d/%m/%Y")) {
 
-  # Argument checking
+  # ARGUMENT CHECKING ----
+  # Object to store check-results
   checks <- checkmate::makeAssertCollection()
+  # Perform assertions
   checkmate::assert_data_frame(data, add = checks)
   checkmate::assert_character(pretext, add = checks)
   checkmate::assert_character(date, add = checks)
+  # Report errors
   checkmate::reportAssertions(checks)
 
-  # Transforms to data frame as this is more flexible than tibble, i.e. accepts adding character to numeric variable
+  # APPEND LINE WITH GENERATED DATE ----
+  # Transforms to data frame as this is more flexible than tibble, i.e. accepts
+  #     adding character to numeric variable
   data <- as.data.frame(data)
 
   # Includes two empty rows at end of data frame
