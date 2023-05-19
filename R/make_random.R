3924b1ca346c4cecebbf29a2a5c771220d54d10a
Petter Hopp <petter.hopp@vetinst.no>
2023-05-19 22:35:53 +0200

    doc: improved help for make_random

--- R/make_random.R
+++ R/make_random.R
@@ -1,17 +1,23 @@
 #' @title Add new column with random numbers
-#' @description Adds new column with random numbers. The function is built to be able to use it in piping.
+#' @description Adds new column with random numbers. The function is built to be
+#'     able to use it in piping.
 #'
 #' @details To make reproducible random numbers the seed can be initialized with a
-#'    specific value. The first time the seed is used, set \code{init_seed = TRUE}.
-#'    Thereafter, use \code{init_seed = FALSE} if more random numbers are generated
-#'    in the session to avoid overlapping random numbers.
+#'     specific value. The first time the seed is used, set \code{init_seed = TRUE}.
+#'     Thereafter, use \code{init_seed = FALSE} if more random numbers are generated
+#'     in the session to avoid overlapping random numbers.
 #'
-#' @param data Data frame
-#' @param colname The name of the new column with the random number.
-#' @param seed The initializing seed
-#' @param init_seed Should the seed be initialized, defaults to FALSE.
+#' @param data [\code{data.frame}]\cr
+#'     Data to which a column with random number should be added.
+#' @param colname [\code{character(1)}]\cr
+#'     The name of the new column with the random number. Defaults to "random".
+#' @param seed [\code{numeric(1)}]\cr
+#'     The initializing seed. Defaults to -1, see
+#'     \ifelse{html}{\code{\link[base]{set.seed}}}{\code{base::set.seed}}.
+#' @param init_seed [\code{logical(1)}]\cr
+#'     Should the seed be initialized. Defaults to \code{FALSE}.
 #'
-#' @return A data frame with a new column with a random variable.
+#' @return \code{data.frame} with a new column with random numbers.
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #' @export
