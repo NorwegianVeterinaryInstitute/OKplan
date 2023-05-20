a9e53ca4744257cec55dfc5a9f1857ce80e2c630
Petter Hopp <Petter.Hopp@vetinst.no>
2023-05-20 12:35:12 +0200

    refactor: uses unicode in test_OK_column_standards

--- tests/testthat/test_OK_column_standards.R
+++ tests/testthat/test_OK_column_standards.R
@@ -6,9 +6,9 @@ test_that("Standard colwidths in Excel for OK-selections", {
   # Generate data frame with column names for table that should be exported to Excel
   # Example with selection of samples collected in herds
   df <- as.data.frame(cbind("ok_aar" = "2021", "rapport" = "Brucellose hos geit, utvalgsliste",
-                            "mt_regionnr" = "M22000", "mt_region" = " Region Øst ",
-                            "mt_avdelingnr" = " M22110", "mt_avdeling" = " Glåmdal og Østerdal ",
-                            "eier_lokalitetnr" = "34343434", "eier_lokalitet" = "Gårdsbruk", "postnr" = "2560", "poststed" = " ALVDAL ",
+                            "mt_regionnr" = "M22000", "mt_region" = " Region \u00D8st ",
+                            "mt_avdelingnr" = " M22110", "mt_avdeling" = " Gl\u00E5mdal og \u00D8sterdal ",
+                            "eier_lokalitetnr" = "34343434", "eier_lokalitet" = "G\u00E5rdsbruk", "postnr" = "2560", "poststed" = " ALVDAL ",
                             "ant_prover" = 30))
 
   # Make a vector with correct column names after translation
@@ -45,13 +45,13 @@ test_that("Standard collabels for OK selections", {
   # Example with selection of samples collected in herds
   df <- as.data.frame(cbind("aar" = "2021", "rapport" = "Paratuberkulose hos storfe, utvalgsliste",
                             "mt_regionnr" = "M21000", "mt_region" = " Region Stor-Oslo",
-                            "mt_avdelingnr" = "M21130", "mt_avdeling" = "Østfold og Follo",
-                            "eier_lokalitetnr" = "3030303030", "eier_lokalitet" = "Gårdsbruk", "postnr" = "1747", "poststed" = "SKJEBERG",
+                            "mt_avdelingnr" = "M21130", "mt_avdeling" = "\u00D8stfold og Follo",
+                            "eier_lokalitetnr" = "3030303030", "eier_lokalitet" = "G\u00E5rdsbruk", "postnr" = "1747", "poststed" = "SKJEBERG",
                             "ant_prover" = 5, "provetakingstidspunkt" = 2))
 
   # Make a vector with correct column names after translation
-  correct_result <- c("År", "Rapport", "MT regionnr", "MT region", "MT avdelingsnr", "MT avdeling", "Produsentnr",
-                      "Viksomhet", "Postnr", "Poststed", "Antall prøver", "Prøvetakingsuke")
+  correct_result <- c("\u00C5r", "Rapport", "MT regionnr", "MT region", "MT avdelingsnr", "MT avdeling", "Produsentnr",
+                      "Viksomhet", "Postnr", "Poststed", "Antall pr\u00F8ver", "Pr\u00F8vetakingsuke")
 
   # Compare Add fylke, current fylkenr and current fylke with correct result
   expect_equal(standardize_columns(data = df,
@@ -69,7 +69,7 @@ test_that("Standard collabels for OK selections", {
 
 
   # Make a vector with correct column names after translation
-  correct_result <- c("MT regionnr", "MT region", "MT avdelingsnr", "MT avdeling", "Autnr", "Slakteri", "Antall prøver")
+  correct_result <- c("MT regionnr", "MT region", "MT avdelingsnr", "MT avdeling", "Autnr", "Slakteri", "Antall pr\u00F8ver")
 
   # Compare Add fylke, current fylkenr and current fylke with correct result
   expect_identical(standardize_columns(data = df,
