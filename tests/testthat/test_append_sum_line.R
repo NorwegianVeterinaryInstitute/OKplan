6d0600a9770cb319485d919b773155c9ed2d8a8b
Petter Hopp <Petter.Hopp@vetinst.no>
2023-05-30 10:35:31 +0200

    style: styled lines in test_append_sum_line

--- tests/testthat/test_append_sum_line.R
+++ tests/testthat/test_append_sum_line.R
@@ -45,7 +45,7 @@ test_that("Append sum in last row", {
 
 
 test_that("Errors for append_sum_line", {
-  
+
   # Make example data
   x <- as.data.frame(cbind("\u00C5r" = 2021, "Rapport" = "Brucellose hos geit, utvalgsliste",
                            "MT regionnr" = "M21000", "MT region" = "Region Stor-Oslo",
@@ -54,13 +54,13 @@ test_that("Errors for append_sum_line", {
                            "Postnr" = "0468", "Poststed" = "OSLO", "Antall prover" = 26))
   # Probably first makes a matrix, therefore "Antall prøver" is character and must be transformed.
   x$`Antall prover` <- as.numeric(x$`Antall prover`)
-  
+
   # Wrong column name
   expect_error(append_sum_line(data = x, column = c("Antall_prover"), pretext = "Sum", position = "left"),
                regexp = "Variable \'column\': Names must be a subset of")
-  
+
   # position not in c("first", "left")
   expect_error(append_sum_line(data = x, column = c("Antall prover"), pretext = "Sum", position = "last"),
                regexp = "Variable \'position\': Must be element of set ")
-  
+
 })
